f39fd5c7b884ce156789b115421dea1a176039bc
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2019-04-22 15:02:59 +0200

    Solve quick sort practice

--- lesson-03/quick-sort-practice.py
+++ lesson-03/quick-sort-practice.py
@@ -2,7 +2,27 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-    return []
+    """Sort array using quicksort"""
+
+    # Define base case
+    if len(array) <= 1: return array
+    
+    # Get pivot value
+    pivot = array[-1]
+
+    # Initialze lists to store results
+    before_pivot = []
+    after_pivot = []
+
+    # Distribute values according to whether they're...
+    for value in array[:-1]:
+        if value < pivot: # ... smaller than the pivot
+            before_pivot.append(value)
+        else: # ... greater than the pivot
+            after_pivot.append(value)
+
+    # Recurse
+    return quicksort(before_pivot) + [pivot] + quicksort(after_pivot)
 
 test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
 print quicksort(test)
