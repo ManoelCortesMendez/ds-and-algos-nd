faa62e9d0fb88623fe3b3a746c10fd8797162f7f
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2019-04-23 10:02:05 +0200

    Solve binary tree practice

--- lesson-05/binary-tree-practice.py
+++ lesson-05/binary-tree-practice.py
@@ -12,23 +12,58 @@ class BinaryTree(object):
         """Return True if the value
         is in the tree, return
         False otherwise."""
-        return False
+
+        # Initiate recursive search starting at root
+        return self.preorder_search(self.root, find_val)
 
     def print_tree(self):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
-        return ""
+
+        # Get string of nodes visited recursively
+        nodes_visited = self.preorder_print(self.root, "")
+
+        # Separate each node with a dash and return
+        return '-'.join(list(nodes_visited))
 
     def preorder_search(self, start, find_val):
         """Helper method - use this to create a 
         recursive search solution."""
+
+        # Define base case
+        if start.value == find_val: return True
+        
+        # If node is parent
+        if start.left or start.right:
+            
+            # Recurse to see if val is in left or right tree
+            left_match = self.preorder_search(start.left, find_val)
+            right_match = self.preorder_search(start.right, find_val)
+            
+            # If there's a match on either side, return true
+            return left_match or right_match
+
+        # Else, return False
         return False
 
     def preorder_print(self, start, traversal):
         """Helper method - use this to create a 
         recursive print solution."""
-        return traversal
+
+        # Add current node value to string of nodes visited
+        nodes_visited = str(start.value)
+
+        # If current node has left child, recurse left
+        if start.left:
+            nodes_visited += self.preorder_print(start.left, traversal)
+
+        # If current node has right child, recurse right
+        if start.right:
+            nodes_visited += self.preorder_print(start.right, traversal)
+
+        # Return string of nodes visited
+        return nodes_visited
 
 
 # Set up tree
